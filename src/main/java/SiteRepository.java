443c06b78fa172925ed5897be21588cd9c7ae6d7
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-05-23 09:48:33 -0300

    barra verde - SiteRepository

---
+++ src/main/java/SiteRepository.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SiteRepository {
+
+    private List<Site> sites = new ArrayList<>();
+
+    void add(Site site) {
+        sites.add(site);
+    }
+
+    boolean isEmpty() {
+        return sites.isEmpty();
+    }
+
+    List<String> getUrls() {
+        return sites.stream().map(Site::getUrl).toList();
+    }
+
+}
--- src/test/java/SiteRepositoryTest.java
+++ src/test/java/SiteRepositoryTest.java
@@ -1,9 +1,30 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class SiteRepositoryTest {
 
+    private static SiteRepository repository;
+    private static final String url = "https://www.google.com.br/";
+
+    @BeforeAll
+    static void setUp() {
+        repository = new SiteRepository();
+        Site site = new Site(url);
+        repository.add(site);
+    }
+
     @Test
-    void test() {
-        SiteRepository repository = new SiteRepository();
+    void checkRepositoryIsNotEmpty() {
+        assertFalse(repository.isEmpty());
+    }
+
+    @Test
+    void getUrls() {
+        assertEquals(List.of(url), repository.getUrls());
     }
 }
